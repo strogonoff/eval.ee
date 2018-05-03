9a4634dc4ab8a2f58e26f9362915e5a14f756fe8
Anton Strogonoff <anton@strogonoff.name>
2018-05-03 18:43:04 +0700

    fix(CI): Initialize NPM package

--- _plugins/webpack.rb
+++ _plugins/webpack.rb
@@ -2,8 +2,8 @@ Jekyll::Hooks.register :site, :after_init do |site|
   unless (File.file?("package.json") and File.directory?("node_modules"))
     p "-- Eva: Initializing Node package…"
 
-    `npm init --yes`
-    `npm install webpack webpack-cli jquery`
+    system("npm init --yes")
+    system("npm install webpack webpack-cli jquery")
   end
 
   if site.theme
