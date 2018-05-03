88591362ecfb16805685da94646db5bf8e1daac7
Anton Strogonoff <anton@strogonoff.name>
2018-05-03 17:06:53 +0700

    Switched to Jekyll & jekyll-theme-eva

---
+++ _plugins/webpack.rb
@@ -0,0 +1,32 @@
+Jekyll::Hooks.register :site, :after_init do |site|
+  unless (File.file?("package.json") and File.directory?("node_modules"))
+    p "-- Eva: Initializing Node package…"
+
+    system("npm init --yes")
+    system("npm install webpack webpack-cli jquery")
+  end
+
+  if site.theme
+    # If site.theme is empty, we’re in theme development mode
+    # and no need to copy
+
+    webpack_config = File.join(site.theme.root, 'webpack.config.js')
+    app = File.join(site.theme.root, 'app')
+
+    unless File.file?('webpack.config.js')
+      FileUtils.copy_entry(webpack_config, File.join(site.source, 'webpack.config.js'))
+    end
+    unless File.directory?('app')
+      FileUtils.copy_entry(app, File.join(site.source, 'app'))
+    end
+  end
+end
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  p "-- Eva: Building assets with Webpack…"
+
+  out_assets_dir = File.join(site.config["destination"], 'assets')
+  system("npx webpack --output-path #{out_assets_dir}")
+
+  p "-- Eva: Build complete"
+end
